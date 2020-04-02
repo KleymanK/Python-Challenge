6a974b9d2b892e18ce9342089f3ad225ba09ce78
Kevin Kleyman <kevinkleyman@icloud.com>
2020-04-01 21:13:43 -0400

    update pypoll file

--- Documents/Python-Challenge/PyPoll/python-challenge.py
+++ Documents/Python-Challenge/PyPoll/python-challenge.py
@@ -3,20 +3,34 @@ import os
 
 csv_file = os.path.join("election_data.csv")
 
+# initialize values
 Total_Votes = 0
-Candidate_Votes = [0, 0, 0, 0]
-Candidates_List = ["Khan","Correy","Li","O'Tooley"]
+candidates_list =[]
+vote_dict = {}
 
+# GET TOTAL votes
+# get total votes by candidates
+# get list of candidates
+# get percentage by candidate
+# get winner
 
 with open (csv_file,'r') as csvwrapper:
     csvreader = csv.reader(csvwrapper, delimiter=',')
-
-Candidate_Choice = int(row[2]) - 1
-
+    header = next(csvreader)
+    
     for row in csvreader:
-        print(row[0],row[1])
         Total_Votes += 1
-        for Candidate_Index in range(len(Candidates_List))
-            Candidates_Count = str(Candidate_Votes[Candidate_Index])
+
+        candidate = row[2]
+
+        if candidate in candidates_list:
+            # add a vote to this candidate's vote count
+            vote_dict[candidate] +=1
+        else:
+            candidates_list.append(candidate)
+            vote_dict[candidate] = 1
+
+
 
 print("Total_Votes = " + str(Total_Votes))
+print(f"Candidates are: {candidates_list}")
